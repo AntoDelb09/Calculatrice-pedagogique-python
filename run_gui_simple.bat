bb897dd47d9f6b7f3cfc8c93359a4bb8e823cd37
AntoDelb09 <antonin.delbouys@safrangroup.com>
2026-03-25 14:42:17 +0100

    Corriger les scripts pour aller dans le répertoire du projet avant exécution

--- run_gui_simple.bat
+++ run_gui_simple.bat
@@ -1,10 +1,10 @@
 @echo off
 REM Script pour lancer l'interface graphique Tkinter simplifiée avec WinPython
 
-REM Aller dans le répertoire du script
-cd /d "%~dp0"
+REM Aller dans le répertoire du projet
+cd /d "C:\Users\L069857\Documents\A_Travail\Projet_Copilot"
 
-REM Lancer l'interface graphique simplifiée avec chemin absolu
-"S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe" "%~dp0src\calculator_gui_simple_tk.py"
+REM Lancer l'interface graphique simplifiée
+"S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe" src\calculator_gui_simple_tk.py
 
 pause
--- run_gui_simple.ps1
+++ run_gui_simple.ps1
@@ -1,9 +1,7 @@
 # Script pour lancer l'interface graphique Tkinter simplifiée avec WinPython
-# Change vers le répertoire du projet
-Push-Location $PSScriptRoot
 
-# Lancer l'interface graphique simplifiée avec chemin absolu
-& "S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe" "$PSScriptRoot\src\calculator_gui_simple_tk.py"
+# Aller dans le répertoire du projet
+Set-Location "C:\Users\L069857\Documents\A_Travail\Projet_Copilot"
 
-# Reviens au répertoire précédent
-Pop-Location
+# Lancer l'interface graphique simplifiée
+& "S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe" src\calculator_gui_simple_tk.py
