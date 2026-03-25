e4c5c06a8f13359487b0ae7155880df6feff6552
AntoDelb09 <antonin.delbouys@safrangroup.com>
2026-03-25 14:33:23 +0100

    Corriger les chemins absolus dans les scripts de lancement

--- run_gui_simple.bat
+++ run_gui_simple.bat
@@ -1,8 +1,10 @@
 @echo off
 REM Script pour lancer l'interface graphique Tkinter simplifiée avec WinPython
+
+REM Aller dans le répertoire du script
 cd /d "%~dp0"
 
-REM Lancer l'interface graphique simplifiée directement avec le chemin complet
-"S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe" src/calculator_gui_simple_tk.py
+REM Lancer l'interface graphique simplifiée avec chemin absolu
+"S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe" "%~dp0src\calculator_gui_simple_tk.py"
 
 pause
--- run_gui_simple.ps1
+++ run_gui_simple.ps1
@@ -2,8 +2,8 @@
 # Change vers le répertoire du projet
 Push-Location $PSScriptRoot
 
-# Lancer l'interface graphique simplifiée directement avec le chemin complet
-& "S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe" src/calculator_gui_simple_tk.py
+# Lancer l'interface graphique simplifiée avec chemin absolu
+& "S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe" "$PSScriptRoot\src\calculator_gui_simple_tk.py"
 
 # Reviens au répertoire précédent
 Pop-Location
