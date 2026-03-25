892010c2ceeae348470e695f9a4d9fe81caef57e
AntoDelb09 <antonin.delbouys@safrangroup.com>
2026-03-25 14:30:59 +0100

    Corriger les scripts de lancement pour gérer les chemins avec espaces et caractères spéciaux

--- run_gui_simple.bat
+++ run_gui_simple.bat
@@ -2,10 +2,7 @@
 REM Script pour lancer l'interface graphique Tkinter simplifiée avec WinPython
 cd /d "%~dp0"
 
-REM Chemin vers Python
-set PYTHON_PATH=S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe
-
-REM Lancer l'interface graphique simplifiée
-"%PYTHON_PATH%" src/calculator_gui_simple_tk.py
+REM Lancer l'interface graphique simplifiée directement avec le chemin complet
+"S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe" src/calculator_gui_simple_tk.py
 
 pause
--- run_gui_simple.ps1
+++ run_gui_simple.ps1
@@ -2,11 +2,8 @@
 # Change vers le répertoire du projet
 Push-Location $PSScriptRoot
 
-# Chemin vers Python
-$pythonPath = "S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe"
-
-# Lancer l'interface graphique simplifiée
-& $pythonPath src/calculator_gui_simple_tk.py
+# Lancer l'interface graphique simplifiée directement avec le chemin complet
+& "S:\Technofan\Blagnac\R&D\BE\01_BE_ELEC\15_Suivi_Outils_Metier_FPGA\ITG\Standards_VHDL\winpython_3x\python-3.7.0\python.exe" src/calculator_gui_simple_tk.py
 
 # Reviens au répertoire précédent
 Pop-Location
